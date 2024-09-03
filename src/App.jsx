f73cc6ad6cd197b578f1674c494fc6253b75f6e6
Adailson <adailson65@gmail.com>
2024-09-03 00:56:38 -0300

    add sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
-import './App.css'
+import './App.css';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
 
 function App() {
-  console.log('test')
-  return 
-    <>Hero</>;
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
